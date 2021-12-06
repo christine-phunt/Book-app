bb38bc8cd5257941418fea6a122ae7cb10b3d85f
EV <ev@apples-MacBook-Pro.local>
2021-12-06 21:29:37 +0800

    Rename todo to book

---
+++ nodejs-api-server/controllers/books.controller.js
@@ -0,0 +1,27 @@
+/**
+ * This is the Books controller.
+ */
+
+module.exports = function(booksModel) {
+
+    /**
+     * Async method that returns a promise with the result or rejects with a message.
+     */
+    const getBooks = () => {
+        return booksModel.getBooks();
+    };
+
+    const createBook = (title) => {
+        return booksModel.createBook(title);
+    }
+
+    const updateBook = (id, changes) => {
+        return booksModel.updateBook(id, changes);
+    }
+
+    return {
+        createBook: createBook,
+        getBooks: getBooks,
+        updateBook: updateBook
+    };
+};
